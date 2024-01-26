540e9cfa9d0113bbba95d384dc79df63afbfecc9
Volodymyr <terces@ukr.net>
2024-01-26 15:52:38 +0200

    26.01.24

--- methods/methods_dict.py
+++ methods/methods_dict.py
@@ -43,3 +43,7 @@ my_dict.update({'b': 3, 'c': 4})  # my_dict = {'a': 1, 'b': 3, 'c': 4}
 keys = ['a', 'b', 'c']
 default_value = 0
 new_dict = dict.fromkeys(keys, default_value)  # {'a': 0, 'b': 0, 'c': 0}
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+print(my_dict.get('a'))  # 1
+print(my_dict.get('d', 'Key not found'))  # Key not found # Вказання значення за замовчуванням
--- practise/training_refresh.py
+++ practise/training_refresh.py
@@ -1,7 +1,18 @@
-import math
-
-def cockroach_speed(s):
-    return round(s * 27.777)
+def correct(s):
+    g = ''
+    for x in s:
+        if x == "1":
+            g = g + 'I'
+        elif x == '5':
+            g = g + 'S'
+        elif x == '0':
+            g = g + 'O'
+        else:
+            g = g + x
+    return g
         
+print(correct("L0ND0N"))  # Виведе: LOND0N
 
-print(cockroach_speed(1.08))
+
+
+
