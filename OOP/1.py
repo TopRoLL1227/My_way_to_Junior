1221d76910461d1c171e56b34137f12d465732fe
Volodymyr <terces@ukr.net>
2024-02-08 17:32:46 +0200

    08.02.24

---
+++ OOP/1.py
@@ -0,0 +1,3 @@
+import Class_Body_scope
+
+print(Class_Body_scope.it1.PYTHON_DEV)
--- practise/training_refresh.py
+++ practise/training_refresh.py
@@ -1,17 +1,39 @@
-class One:
-    def __init__(self, x, z, y):
-        self.x = x
-        self.z = z
-        self.y = y
-    
+class DepartamentIT:
+    PYTHON_DEV = 4
+    GO_DEV = 3
+    REACT_DEV = 2
+
+    def first_round(self):
+        print(self.PYTHON_DEV, self.GO_DEV, self.REACT_DEV)
+
+    def second_round(self):
+        print(DepartamentIT.PYTHON_DEV, DepartamentIT.GO_DEV, DepartamentIT.REACT_DEV)
+
+    @property
+    def third_round(self):
+        print(self.PYTHON_DEV, self.GO_DEV, self.REACT_DEV)
+
     @classmethod
-    def two(cls, a):
-        return cls(*a.split(','))
+    def fourth_round(cls):
+        return cls.PYTHON_DEV, cls.GO_DEV, cls.REACT_DEV
     
-asd = One.two("1,2,3")
-print(asd.x, asd.z, asd.y)
+    @staticmethod
+    def fifth_found():
+        return DepartamentIT.PYTHON_DEV, DepartamentIT.GO_DEV, DepartamentIT.REACT_DEV
+    
+    def bulba(self):
+        self.GO_DEV = self.GO_DEV
+    
 
 
+asd = DepartamentIT()
+asd.first_round()
+asd.second_round()
+asd.third_round
+print(asd.fourth_round())
+print(asd.fifth_found())
 
-s = '5,6,7'
-print(*s)
+print(DepartamentIT.GO_DEV)
+asd.bulba = 5
+print(asd.bulba)
+
